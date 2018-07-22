fea0e0519ca8ba0a3911bd8cda6cc1fc2ae091f9
dmgardiner25 <dmgardiner25@gmail.com>
2018-07-22 00:52:09 -0500

    Add photo acknowledgement

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,13 +25,15 @@ class HomeContent extends React.Component {
     return (
       <div className='home-content'>
         <div className='stacked-text'>
-          <h1>Hey there, I'm David Gardiner</h1>
+          <h1>Hey there, my name is David Gardiner</h1>
           <h2>
             I'm a Computer Science student at Missouri S&T <br />
-            with a passion for anything and everything that <br /> involves programming.
+            with a passion for anything and everything <br />
+            that involves programming.
           </h2>
         </div>
         <img className='profile' src={require('./../images/suit_green_cropped.jpg')} alt='profile'/>
+        <p>Like this photo? Check out more of Amanda Frazier's work <a target="_blank" rel="noopener noreferrer" href="http://amandafrazierwebdev.com">here</a>.</p>
       </div>
     );
   }
