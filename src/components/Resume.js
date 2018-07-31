b263d56bffebc03db102ad6887fa515c239d3a2e
Gardiner <s0045844@rgare.net>
2018-07-31 11:34:55 -0500

    Change resume to .jpg for sake of speed

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { loadpdf } from './../util/loadpdf.js';
 import './../styles/Resume.css'
 
 class Resume extends React.Component {
   render() {
-    loadpdf();
     return (
       <div className='resume'>
         <Helmet>
           <title>David Gardiner | Resume</title>
         </Helmet>
         <div className='resume-content'>
-          <canvas id="the-canvas"></canvas>
+          <img className='resume' src={require('./../images/DavidGardinerResume.jpg')} alt='resume' />
         </div>
       </div>
     );
